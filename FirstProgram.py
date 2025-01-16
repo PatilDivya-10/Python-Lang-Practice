0d6625e22b49fada43456a5d9d993a8855b69993
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-16 11:43:58 +0530

    Adding the first program of print function

---
+++ FirstProgram.py
@@ -0,0 +1,8 @@
+print("Hello World!!")
+print("My name is Divya.")
+print("My age is 20")
+#print the statements or strings
+print("My name is Divya.","My age is 20")
+#printing the numbers
+print(23)
+print(23+38)
