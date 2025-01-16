2615085e15aea450df8cb5b88bacda7f2a1fe2a2
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-16 11:55:14 +0530

    rules of var and print

---
+++ Variables.py
@@ -0,0 +1,13 @@
+name="Divya" #string data type which defines in double quote
+age=20 #integer datatype
+price=24.45 #float variable
+#print the var name without using dq
+print(name)
+print(age)
+print(price)
+#printing with string and variable
+print("My name is:",name)
+print("My age is:",age)
+#assignment right to left
+age= 21
+print(age)
