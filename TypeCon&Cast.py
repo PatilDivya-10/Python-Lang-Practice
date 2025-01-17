40295d7e14bfc1c08f1974f9bbb495e6ceb7e8d9
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-18 00:09:31 +0530

    Type conversion and casting

---
+++ TypeCon&Cast.py
@@ -0,0 +1,13 @@
+#type conversion
+a = 2
+b = 4.25
+
+sum =  a + b #automatically converting int to float(superior)
+print(sum) #float value
+
+#Type Casting
+a,b= 3.12,"2"
+c=int(b) #converting into int
+print(a+c)
+d=float(b) #converting into float
+e=str(a)#converting into string
