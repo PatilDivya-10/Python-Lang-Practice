177c26f288dbe0928ab2323e7bb9de8aa2f08c44
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-17 23:12:13 +0530

    Comparison Operatprs in Python

--- Operators.py
+++ Operators.py
@@ -1,11 +1,25 @@
-#Arithmetic Operator
 a = 5
 b = 3
-
+#Arithmetic Operator
 print(a + b)
 print(a - b)
 print(a * b)
 print(a / b) #always comes in float datatype
 print(a % b) #modulus operator for reminder
+print(a ** b) #a^b Power of
+
+
+#Relational | Comparison Operator
+a = 50
+b = 20
+#return boolean  value
+print(a == b) #False
+print(a != b) #True
+print(a > b) #True
+print(a >= b) #True
+print(a < b) #False
+print(a <= b) #False
+
+
 
 
