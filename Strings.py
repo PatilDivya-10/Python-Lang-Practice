9baa1e7004cfb408c99addb9fd287dee617d07bd
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-19 22:32:33 +0530

    Slicing in string

--- Strings.py
+++ Strings.py
@@ -9,4 +9,14 @@ print(len(str3))
 #access ch with index no but can't modify
 ch=str1[3]
 print(ch)
+#slicing in string
+st = "Divya Patil"
+print(st[0:4])
+print(st[1:6])
+print(st[5:len(st)])
+print(st[3:])#last index
+print(st[:5])#start from 0
+#slicing negative (reverse)
+s="Apple"
+print(s[-3:-1]) #pl <- op
 
