d425f08daa831dd003d732acb66e1bc2cc86eb8c
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-19 23:49:58 +0530

    Conditional if elif and else statement

---
+++ ConditionalStat.py
@@ -0,0 +1,20 @@
+#Conditional statement
+#if else statement
+age = 21
+if(age >= 18):
+    print("Can vote an drive")
+else:
+    print("You can't!!")
+
+#if elif else statement
+light ="green"
+
+if(light == "red"):
+    print("Stop")
+elif(light =="yellow"):
+    print("Look")
+elif(light =="green"):
+    print("Go")
+else:
+    print("Enter valid color value")
+
