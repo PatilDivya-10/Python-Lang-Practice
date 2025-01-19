60934f9422d06242e1bbbbe6cb8d8e5acd1d94d9
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-19 20:01:36 +0530

    Practice programs Basic

---
+++ Pract_prog1.py
@@ -0,0 +1,18 @@
+#Area of Square
+side = int(input("Enter the Side of the Square:"))
+area = side  * side
+print("Area:", area)
+
+#printing the avg of floating numbers
+a = float (input("Enter first:"))
+b = float (input("Enter Second :"))
+avg = (a+b)/2
+print("Average:",avg)
+
+#WAP to input  2 int numbers, a and b.
+#print True id a is greater than or equal to b. If not print false
+
+x = int(input("Enter First"))
+y = int(input("Enter Second:"))
+
+print(a>=b)
