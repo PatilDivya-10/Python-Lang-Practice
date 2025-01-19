d63282a5b289d6d4bb67e752b2d2e03d58bea557
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-19 23:15:10 +0530

    Practice program str

---
+++ Pract_prog2.py
@@ -0,0 +1,7 @@
+#WAP to input user's first name and print its length
+name = input("Enter ur name:")
+print("Length of ur name is ", len(name))
+
+#WAP to find the occurance if $ in the string
+str = "Hi, $ I m the $ symbol."
+print("Count of $ symbol is ",str.count("$"))
