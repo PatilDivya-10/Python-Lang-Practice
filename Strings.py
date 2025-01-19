1e92c119168b6abd26ca3d5a9cbbe7be24e440dc
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-19 22:24:21 +0530

    String len() and concat (+)

---
+++ Strings.py
@@ -0,0 +1,12 @@
+#ways to store string in var
+str1= "This is string"
+str2='single quote'
+str3="""This is string"""
+#Operations -Concatenation (+ operator)
+print(str1+str2)
+#length() operation 
+print(len(str3))
+#access ch with index no but can't modify
+ch=str1[3]
+print(ch)
+
