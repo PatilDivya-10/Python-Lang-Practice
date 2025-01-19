80d783cd7b3c69c9e7752539125cc53e3ff50017
PatilDivya-10 <divya.jadhavpatil2005@gmail.com>
2025-01-19 19:47:39 +0530

    Input statement

---
+++ InputStat.py
@@ -0,0 +1,18 @@
+#input() mainly used to get the input from the user as a string
+name = input("Enter Ur Name:")
+print(name)
+#each entered value stired as string
+val=input("Enter some value:")
+print(type(val))
+#we use typecast for it
+val1 = int(input("Enter  Integer Value"))
+print(val1)
+
+#basic program
+name = input("Enter Name:")
+age = int(input("Enter Age:"))
+marks = float(input("Enter Marks:"))
+
+print(name)
+print(age)
+print(marks)
